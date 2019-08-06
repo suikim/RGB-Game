5635b6539e0f01eba503fa6a901807d074b5753a
suikim <susaninhakim@gmail.com>
2019-08-05 17:36:24 -0700

    correct-color-event-listener
    added event listeners to colored squares, function to randomly select one square to have correct color, functionality so that event listener prints "correct" into console if correct colore square is clicked.

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,4 @@
-
+//***   Element Selectors   ***//
 //element selector for RGB values for color we're looking for in game
 var valueR = document.getElementById("numR");
 var valueG = document.getElementById("numG");
@@ -8,37 +8,58 @@ var reset = document.getElementById("reset");
 //element selector for all colored squares
 var squares = document.querySelectorAll(".square");
 
-//New Colors button - changes color of squares and the RGB number player is looking for
-reset.addEventListener("click", function(){ rgbNumber(); colorSquare(); });
+
 
 //set RGB number when page loads
 rgbNumber();
-
 //color squares when page loads
-colorSquare();
+colorSquare(valueR.textContent, valueG.textContent, valueB.textContent);
 
 
+//*** Event Listeners   ***//
+//New Colors button - changes color of squares and the RGB number player is looking for
+reset.addEventListener("click", function(){ rgbNumber(); colorSquare(valueR.textContent, valueG.textContent, valueB.textContent); });
+//check is clicked square is correct one
+for (var i = 0; i < squares.length; i++) {
+	(function(index) {
+		squares[i].addEventListener("click", function(){
+			if( squares[index].style.backgroundColor == writeRGB(valueR.textContent, valueG.textContent, valueB.textContent) ){
+				console.log("correct!");
+			}
+		});
+	})(i);
+}
+
+ 
 
 //***   Functions   ***//
 
 //set RGB value we're looking for
 function rgbNumber(){
-	valueR.textContent=getRandomInt(0, 255);
-	valueG.textContent=getRandomInt(0, 255);
-	valueB.textContent=getRandomInt(0, 255);
+	valueR.textContent=getRandomInt(0, 256);
+	valueG.textContent=getRandomInt(0, 256);
+	valueB.textContent=getRandomInt(0, 256);
 }
 
 //update each square to have different color
-function colorSquare(){
+function colorSquare(valR, valG, valB){
+	var correct = getRandomInt(0,6); //pick random square to be correct square
+	squares[correct].style.backgroundColor= writeRGB(valR, valG, valB);
 	for (var i = 0; i < squares.length; i++) {
-		squares[i].style.backgroundColor= "rgb(" + getRandomInt(0, 255) + ", " + 
-		getRandomInt(0, 255) + ", " + getRandomInt(0, 255) + ")" ;
+		if(i != correct){
+			squares[i].style.backgroundColor= writeRGB(getRandomInt(0, 256), getRandomInt(0, 256), getRandomInt(0, 256));
+		}
 	}
 }
 
-//random integer between 2 values - from MDN website
+//random integer between 2 values - code from MDN website
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+//creates textContent for rgb numbers
+function writeRGB(valR, valG, valB){
+	return "rgb(" + valR + ", " + valG + ", " + valB + ")";
 }
