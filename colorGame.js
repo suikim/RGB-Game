6405de86fc95262a11f4ea92f751d91856485d32
suikim <susaninhakim@gmail.com>
2019-08-12 18:08:09 -0700

    easy-hard-mode-functionality
    Added ids to easy and hard buttons.  Added element selelectors and event listeners to easy and hard buttons. Also added element selector for container that holds all squares. Event listeners for easy/hard buttons changes number of colored squares, calls resetValues(), and adds event listeners to squares.
    Made separate function to add event listeners to squares.
    Made a new function for repetitive lines: resetting RGB number, recoloring squares, changing text for reset button and 'correct/incorrect' message, resetting 'won' boolean back to false.

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,5 @@
 //***   Element Selectors   ***//
+
 //element selector for RGB values for color we're looking for in game
 var valueR = document.getElementById("numR");
 var valueG = document.getElementById("numG");
@@ -11,47 +12,49 @@ var squares = document.querySelectorAll(".square");
 var alert = document.getElementById("message");
 //boolean value to check if player selected right color
 var won = false;
+//element selector for easy mode button
+var easyButton = document.getElementById("easy");
+//element selector for hard mode button
+var hardButton = document.getElementById("hard");
+//element selector for container that holds all squares
+var visibleSquares = document.getElementById("container");
 
 
+//*** Call functions when page initially loads ***//
 
 //set RGB number when page loads
 rgbNumber();
 //color squares when page loads
 colorSquare(valueR.textContent, valueG.textContent, valueB.textContent);
+//add event listeners to colored squares
+squareClickable();
 
 
 //*** Event Listeners   ***//
+
 //New Colors button - changes color of squares and the RGB number player is looking for, reset won boolean to false
 reset.addEventListener("click", function(){ 
-	rgbNumber(); 
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].classList.remove("addFadeBlack");
 	}
-	colorSquare(valueR.textContent, valueG.textContent, valueB.textContent); 
-	reset.textContent="New Colors";
-	message.textContent="";
-	won = false;
+	resetValues();
 });
 
-//check is clicked square is correct one
-for (var i = 0; i < squares.length; i++) {
-	(function(index) {
-		squares[i].addEventListener("click", function(){
-			if( squares[index].style.backgroundColor == writeRGB(valueR.textContent, valueG.textContent, valueB.textContent) ){
-				message.textContent = "Correct!"
-				won = true;
-				reset.textContent="Try Again?";
-			}
-			else{
-				if(won == false){
-					message.textContent="Incorrect!"
-					squares[index].classList.add("addFadeBlack");
-					//squares[index].style.backgroundColor = "#232323";
-				}
-			}
-		});
-	})(i);
-}
+//Easy Mode Button
+easyButton.addEventListener("click", function(){
+	visibleSquares.innerHTML = "<div class=\"square\"> </div> <div class=\"square\"> </div> <div class=\"square\"> </div>";
+	squares = document.querySelectorAll(".square");
+	resetValues();
+	squareClickable();
+});
+
+//Hard Mode Button
+hardButton.addEventListener("click", function(){
+	visibleSquares.innerHTML = "<div class=\"square\"> </div> <div class=\"square\"> </div> <div class=\"square\"> </div> <div class=\"square\"> </div> <div class=\"square\"> </div> <div class=\"square\"> </div>";
+	squares = document.querySelectorAll(".square");
+	resetValues();
+	squareClickable();
+});
 
 
 //***   Functions   ***//
@@ -65,12 +68,34 @@ function rgbNumber(){
 
 //update each square to have different color
 function colorSquare(valR, valG, valB){
-	var correct = getRandomInt(0,6); //pick random square to be correct square
+	var correct = getRandomInt(0,squares.length); //pick random square to be correct square
 	squares[correct].style.backgroundColor= writeRGB(valR, valG, valB);
 	for (var i = 0; i < squares.length; i++) {
 		if(i != correct){
 			squares[i].style.backgroundColor= writeRGB(getRandomInt(0, 256), getRandomInt(0, 256), getRandomInt(0, 256));
 		}
+	}
+}
+
+//check if clicked square is correct one
+function squareClickable(){
+	for (var i = 0; i < squares.length; i++) {
+		(function(index) {
+			squares[i].addEventListener("click", function(){
+				if( squares[index].style.backgroundColor == writeRGB(valueR.textContent, valueG.textContent, valueB.textContent) ){
+					message.textContent = "Correct!";
+					won = true;
+					reset.textContent="Try Again?";
+				}
+				else{
+					if(won == false){
+						message.textContent="Incorrect!"
+						squares[index].classList.add("addFadeBlack");
+						//squares[index].style.backgroundColor = "#232323";
+					}
+				}
+			});
+		})(i);
 	}
 }
 
@@ -84,4 +109,13 @@ function getRandomInt(min, max) {
 //creates textContent for rgb numbers
 function writeRGB(valR, valG, valB){
 	return "rgb(" + valR + ", " + valG + ", " + valB + ")";
+}
+
+//function for repetitive actions: reset RGB number, recoloring squares, change text for reset button and message, reset won boolean to false
+function resetValues(){
+	rgbNumber();
+	colorSquare(valueR.textContent, valueG.textContent, valueB.textContent); 
+	reset.textContent="New Colors";
+	message.textContent="";
+	won = false;
 }
